65a4b70380c7577c90edf4e2cc1f2f7c4412e95e
JameelGharra <jameelgharra@gmail.com>
2022-09-24 18:59:32 +0300

    Cleaned up the named routes, modified debugging method (print->log), added functionality of logging out.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -14,12 +15,13 @@ void main() {
   runApp(MaterialApp(
     title: 'Notes App',
     theme: ThemeData(
-      primarySwatch: Colors.blue,
+      primarySwatch: Colors.red,
     ),
     home: const HomePage(),
     routes: {
-      '/login/': (context) => const LoginView(),
-      '/register/': (context) => const RegisterView()
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
     },
   ));
 }
@@ -75,7 +77,12 @@ class _NotesViewState extends State<NotesView> {
               case MenuAction.logout:
                 {
                   final shouldLogout = await showLogOutDialog(context);
-                  devtools.log(shouldLogout.toString());
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                  }
                   break;
                 }
             }
